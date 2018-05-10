4984c72d72b630c34b9ec1276e6b3b2a966e26d2
avinash.yadav <avinash.yadav@wns.com>
2018-05-10 17:19:03 +0530

    added user account setup and modification functionality

--- controllers/authController.js
+++ controllers/authController.js
@@ -12,3 +12,14 @@ exports.logout = (req, res) => {
     req.flash('success', 'You are now logged out!');
     res.redirect('/');
 }
+
+exports.isLoggedIn = (req, res, next) => {
+    // check if the user is authenticated
+    if (req.isAuthenticated()) {
+        next();
+        return;
+    }
+    req.flash('error', 'Oops! You must be logged in to do that!');
+    res.redirect('/login');
+}
+
