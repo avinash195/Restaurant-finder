6010d938ee9d604af75dcc37abe757c9427c5fe4
avinash.yadav <avinash.yadav@wns.com>
2018-05-10 19:31:10 +0530

    work in progress on password reset flow

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,7 @@
 const passport = require('passport');
+const crypto = require('crypto');
+const mongoose = require('mongoose');
+const User = mongoose.model('User');
 
 exports.login = passport.authenticate('local', {
     failureRedirect: '/login',
@@ -21,5 +24,23 @@ exports.isLoggedIn = (req, res, next) => {
     }
     req.flash('error', 'Oops! You must be logged in to do that!');
     res.redirect('/login');
+}
+
+exports.forgot = async (req, res) => {
+    // 1. See if a user with that email exists
+    const user = await User.findOne({ email: req.body.email });
+    if(!user) {
+        req.flash('error', 'No account with that email exists!');
+        return res.redirect('/login');
+    }
+    // 2. Set reset tokens and expiry on their account
+    user.resetPasswordToken = crypto.randomBytes(20).toString('hex');
+    user.resetPasswordExpires = Date.now() + 3600000; // 1 hour from now
+    await user.save();
+    // 3. Send them an email with the token
+    const resetURL = `http://${req.headers.host}.account/reset/${user.resetPasswordToken}`;  
+    req.flash('success', 'You have been emailed a password reset link');
+    // 4. redirect to login page
+
 }
 
--- models/User.js
+++ models/User.js
@@ -19,13 +19,16 @@ const userSchema = new Schema({
     type: String,
     required: 'Please supply a name',
     trim: true
-  }
+  },
+  resetPasswordToken: String,
+  resetPasswordExpires: Date
 })
 
 userSchema.virtual('gravatar').get(function() {
   const hash = md5(this.email);
   return `https://gravatar.com/avatar/${hash}`;
 })
+
 userSchema.plugin(passportLocalMongoose, { usernameField: 'email' });
 userSchema.plugin(mongodbErrorHandler);
 
