1f3c8134f1f1ee2515414c9cd5f1be59ba8db660
avinash195 <avinashsngh735@gmail.com>
2018-05-20 17:06:31 +0530

    worked on listing top rated stores

--- models/Store.js
+++ models/Store.js
@@ -76,6 +76,27 @@ storeSchema.statics.getTagsList = function() {
   ]);
 }
 
+storeSchema.statics.getTopStores = function() {
+  return this.aggregate([
+    // Lookup Stores and populate their reviews
+    { $lookup: { from: 'reviews', localField: '_id', foreignField: 'store', as: 'reviews' }},
+    // filter for only items that have 2 or more reviews
+    { $match: { 'reviews.1': { $exists: true } } },
+    // Add the average reviews field, if you're on greater than mongodb 3.2 then no need to project fields
+    { $project: {
+      photo: '$$ROOT.photo',
+      name: '$$ROOT.name',
+      reviews: '$$ROOT.reviews',
+      slug: '$$ROOT.slug',
+      averageRating: { $avg: '$reviews.rating' }
+    } },
+    // sort it by our new field, highest reviews first
+    { $sort: { averageRating: -1 }},
+    // limit to at most 10
+    { $limit: 10 }
+  ]);
+}
+
 // find reviews where store id property === review store property
 storeSchema.virtual('reviews', {
   ref: 'Review', 
@@ -83,5 +104,12 @@ storeSchema.virtual('reviews', {
   foreignField: 'store'
 })
 
+function autopopulate(next) {
+  this.populate('reviews');
+  next();
+}
+
+storeSchema.pre('find', autopopulate);
+storeSchema.pre('findOne', autopopulate);
 
 module.exports = mongoose.model('Store', storeSchema);
